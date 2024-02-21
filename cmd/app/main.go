3dd4e2aede8741c5e109cfaac0e63ad23b77f129
D1mitrii <dyusmirnov@stud.etu.ru>
2024-02-21 23:19:06 +0300

    feat: add gracefull shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"auth/internal/config"
 	"auth/pkg/logger"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	config := config.MustLoad()
 	log := logger.SetupLogger(config.Env)
 	log.Info("Success startup")
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	sig := <-quit
+	log.Info("Shutdown application", slog.String("signal", sig.String()))
 }
