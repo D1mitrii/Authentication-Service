849f2e19d8780a306e996fd23db786fe9adf870b
D1mitrii <dyusmirnov@stud.etu.ru>
2024-02-23 15:41:34 +0300

    feat: add  simple http server with chi router

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
+	"auth/internal/app"
 	"auth/internal/config"
-	"auth/pkg/logger"
-	"log/slog"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func main() {
 	config := config.MustLoad()
-	log := logger.SetupLogger(config.Env)
-	log.Info("Success startup")
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
-	sig := <-quit
-	log.Info("Shutdown application", slog.String("signal", sig.String()))
+	app.Run(config)
 }
